8dcc8188d75e74805de67c500b5697608caf91dc
Ronna Faye Jamola <jamolar@ktdp.local>
2018-03-28 17:42:00 +0800

    Initial validation of Sign In

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,11 +3,14 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable} from 'rxjs/Rx';
 import { Router } from '@angular/router';
+import { Subject}    from 'rxjs/Subject';
 
 export class AuthService{
 
     token: string;
     uid: string;
+    private signInIndicator = new Subject<string>();
+    signInIndicator$ = this.signInIndicator.asObservable();
     constructor(private router: Router, private afAuth: AngularFireAuth,
         private afs: AngularFirestore){
     }
@@ -20,24 +23,23 @@ export class AuthService{
         )
     }
 
-    signinUser(email: string , password:string){
+    signinUser(email: string , password:string):Observable<any>{
         this.afAuth.auth.signInWithEmailAndPassword(email, password)
          .then(
              response => {
                 this.afAuth.auth.currentUser.getIdToken()
                     .then(
                         (token: string) => this.token = token
-                    
-                    )
-                   
+                    )              
                     console.log("Successful sign in");
-                    
-                    this.router.navigate(['/home']); //if successfuly logged-in, redirect to Home page
+                    this.router.navigate(['/home']);
              }    
-         ).catch(
-             error=> console.log(error)
-         );
-         
+         )
+         .catch((err:Response) => {
+            return Observable.throw(err);
+         });
+         this.signInIndicator.next(this.token);
+         return  this.signInIndicator$;
      }
 
      getToken(){
