db0d2c0f28fa0d7ebfdcb72cbb00691d7352096f
Ronna Faye Jamola <jamolar@ktdp.local>
2018-04-17 17:47:48 +0800

    Added fix for private auth service.

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -18,7 +18,7 @@ export class SigninComponent implements OnInit {
 
   constructor( 
     private router: Router, 
-    private authService: AuthService){
+    public authService: AuthService){
 
    }
 
