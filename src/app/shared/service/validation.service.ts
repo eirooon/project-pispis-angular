8771b88984f368ebc7f3ec2e78ab370ac2866898
eirooon <erron.sevilla@gmail.com>
2018-04-21 23:25:50 +0800

    enhancement after 1st release

--- src/app/shared/service/validation.service.ts
+++ src/app/shared/service/validation.service.ts
@@ -1,7 +1,7 @@
 export class ValidationService {
     static getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
         let config = {
-            'required': 'This is a required field',
+            'required': 'This is a required.',
             'invalidCreditCard': 'Is invalid credit card number',
             'invalidEmailAddress': 'Invalid email address',
             'invalidPassword': 'Invalid password. Password must be at least 6 characters long, and contain a number.',
