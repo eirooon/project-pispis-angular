f85213b1f1e985bde8082dc7c4e9c6fa56229b56
Anthony Sevilla <sevillaa@ktdp.local>
2018-11-07 10:40:09 +0800

    Prescription. Partial 2

--- src/app/modules/patient/patient-details/pd-consultation-prescription/pd-consultation-prescription.component.ts
+++ src/app/modules/patient/patient-details/pd-consultation-prescription/pd-consultation-prescription.component.ts
@@ -5,6 +5,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Logger } from '../../../../shared/service/logger.service';
 import { SuiModalService, TemplateModalConfig, ModalTemplate } from 'ng2-semantic-ui';
 
+import { Patient } from '../../../../shared/models/patient';
+import { PatientService } from '../../../../shared/service/patient.service';
+
 import { ConsultationTextModel } from '../../../../shared/models/consulationModel';
 import { MedicineModel } from '../../../../shared/models/medicineModel';
 export interface IContext {
@@ -21,14 +24,17 @@ export class PdConsultationPrescriptionComponent implements OnInit {
   public modalTemplate: ModalTemplate<IContext, string, string>
 
   CLASSNAME: string = this.constructor.name;
+  
   consultationText: ConsultationTextModel;
-  medicineModel: MedicineModel[];
+  medicineModel: MedicineModel[] = [];
+  patient: Patient;
 
   constructor(
     private location: Location,
     private router: Router,
     public modalService: SuiModalService,
-    private logger: Logger
+    private logger: Logger,
+    private patientService: PatientService,
   ) {
     this.ngOnInit();
   }
@@ -47,7 +53,7 @@ export class PdConsultationPrescriptionComponent implements OnInit {
     dose: new FormControl("", Validators.required),
     form: new FormControl("", Validators.required),
     qty: new FormControl("", Validators.required),
-    notes: new FormControl(""),
+    note: new FormControl(""),
     frequency: new FormControl(""),
     startDate: new FormControl(""),
     endDate: new FormControl(""),
@@ -59,6 +65,8 @@ export class PdConsultationPrescriptionComponent implements OnInit {
    * @return void
    */
   ngOnInit() {
+    this.logger.info(this.CLASSNAME, "ngOnInit", "Initial Load");
+    this.patient = this.patientService.getPatientById(); //THIS PART ANG GA ERROR.
   }
 
   /**
@@ -67,7 +75,8 @@ export class PdConsultationPrescriptionComponent implements OnInit {
    * @return void
    */
   goBack() {
-    this.location.back();
+    // this.location.back();
+    console.log(this.medicineModel);
   }
 
   /**
@@ -88,10 +97,53 @@ export class PdConsultationPrescriptionComponent implements OnInit {
   }
 
   addMedicine(){
-    console.log(this.medicineForm.value.medicineName);
+    if (this.medicineForm.valid) {
+      this.medicineModel.push({
+        medicine: this.medicineForm.value.medicineName, 
+        brand: this.medicineForm.value.brandName, 
+        generic: this.medicineForm.value.genericName, 
+        dose: this.medicineForm.value.dose,
+        form: this.medicineForm.value.form,
+        qty: this.medicineForm.value.qty,
+        note: this.medicineForm.value.note,
+        frequency: this.medicineForm.value.frequency,
+        startDate: this.medicineForm.value.startDate,
+        endDate: this.medicineForm.value.endDate});
+    } else {
+      this.logger.error(this.CLASSNAME, "addMedicine", "Error: Form is invalid");
+    }
+    console.log(this.medicineForm.value.medicineName, 
+                this.medicineForm.value.brandName, 
+                this.medicineForm.value.genericName, 
+                this.medicineForm.value.dose,
+                this.medicineForm.value.form,
+                this.medicineForm.value.qty,
+                this.medicineForm.value.note,
+                this.medicineForm.value.frequency,
+                this.medicineForm.value.startDate);
   }
 
   addConsultationPrescription(){
-    
+    console.log('DPUTA KA');
+    // if (this.prescriptionForm.valid) {
+
+    // } else {
+    //   this.logger.error(this.CLASSNAME, "addConsultationText", "Error: Form is invalid");
+    // }
+    // this.consultationText.type = "Prescription";
+    // this.consultationText.idPatient = this.patient.id;
+    // this.consultationText.clinicname = this.prescriptionForm.value.clinicname,
+    // this.consultationText.date = this.prescriptionForm.value.date;
+    // this.consultationText.text = this.prescriptionForm.value.text;
+    // this.consultationText.patientType = this.prescriptionForm.value.patientType;
+    // this.consultationService.addConsultationText(this.consultationText);
+    // this.logger.info(this.CLASSNAME, "addConsultationText", "Clinic name: [" + this.clinicname + "] Adding Consulation done.");
+    // this.router.navigateByUrl['/patient/patient-details'];
+    console.log("Prescription",
+                this.patient.id,
+                this.prescriptionForm.value.clinicname,
+                this.prescriptionForm.value.date,
+                this.prescriptionForm.value.text,
+                this.prescriptionForm.value.patientType);
   }
 }
